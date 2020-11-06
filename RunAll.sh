81ee47407c44ede64b52435adab889e884a21b4e
nandlalyadav57 <33649510+nandlalyadav57@users.noreply.github.com>
2020-11-06 15:15:07 +0530

    aavs
    vdsvs

--- RunAll.sh
+++ RunAll.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd /tmp/elastic
 chmod 400 /tmp/elastic/n.pem
-sudo apt update -y 
+sudo apt update -y
 sudo apt-get upgrade -y
-sudo apt install ansible -y  
+sudo apt install ansible -y
+sudo apt install python3 -y
+apt install python3-pip -y
+pip3 install boto
 bash ./2.sh
 bash ./3.sh
 bash ./4.sh
